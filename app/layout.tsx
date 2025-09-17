45cd4c6fcd989fe8754ae3bec2700aedcd8b7afc
Code-W-A <radu.p1995@yahoo.com>
2025-09-17 15:00:08 +0300

    asda

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="">
       <body className={`font-sans ${inter.variable} antialiased`}>
         <PasswordGate>
           <Navbar />
